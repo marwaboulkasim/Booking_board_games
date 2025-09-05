89474f2f4d636a4dc96365988d877fd2cb818baa
Marwa <marwa-aboulkasim@hotmail.fr>
2025-09-05 10:20:07 +0200

    j'ai rajouter les chemins urls view et setting

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'tables_app',  # ✅ ton app doit être listée ici
 ]
+
+
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
