a6981962dd0d83a51d7004cf3df9f591e18934bf
Marwa <marwa-aboulkasim@hotmail.fr>
2025-09-08 11:24:12 +0200

    maj

--- booking/tables_app/views.py
+++ booking/tables_app/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'tables_app/home.html')
+
--- booking/users_app/views.py
+++ booking/users_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
-
+#marwa
 def register_view(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)  # <== ton form personnalisé
+        form = CustomUserCreationForm(request.POST)  
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -30,3 +30,4 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+#fin marwa
