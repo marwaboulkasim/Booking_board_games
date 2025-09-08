e73a49d94253297d209cfb333bb640c5e130768b
GaetanCSimplon <gcourtieupro@gmail.com>
2025-09-08 15:25:21 +0200

    App fonctionnelle

--- booking/tables_app/urls.py
+++ booking/tables_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'tables_app'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('calendar/', views.calendar_view, name='calendar')
 ]
